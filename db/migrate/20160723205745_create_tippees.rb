9fb1c3302b511b1b9a86a954fdcece88d7977c0e
Chris Otto <email": null>
2016-07-23 16:53:19 -0700

    Change 'string' to 'text' in migrations

--- db/migrate/20160723205745_create_tippees.rb
+++ db/migrate/20160723205745_create_tippees.rb
@@ -1,10 +1,10 @@
 class CreateTippees < ActiveRecord::Migration[5.0]
   def change
     create_table :tippees do |t|
-      t.string :first_name, null: false
-      t.string :last_name, null: false
-      t.string :photo_url
-      t.string :payment_url, null: false
+      t.text :first_name, null: false
+      t.text :last_name, null: false
+      t.text :photo_url
+      t.text :payment_url, null: false
 
       t.timestamps null: false
     end
